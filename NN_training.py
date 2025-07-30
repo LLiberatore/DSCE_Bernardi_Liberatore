6037631db5399b44ba000066d3ddaf83a0998958
LLiberatore <lucaliberatore3@gmail.com>
2025-07-30 20:46:15 +0200

    NN defined
    NN definition introduced; the number of layers, number of neurons, and type of activation function still need to be determined.

--- NN_training.py
+++ NN_training.py
@@ -21,3 +21,11 @@ X_test_scaled = scaler_X.transform(X_test)
 scaler_Y = MinMaxScaler()
 Y_train_scaled = scaler_Y.fit_transform(Y_train)
 Y_test_scaled = scaler_Y.transform(Y_test)
+
+# Define network
+net = tf.keras.models.Sequential([
+    tf.keras.layers.Input(shape=(X_train.shape[1],)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(Y_labels.shape[1], activation='linear'),
+])
