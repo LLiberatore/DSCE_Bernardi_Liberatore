3d4d04a2674f1570b4c3f2813a639295fce107ee
LLiberatore <lucaliberatore3@gmail.com>
2025-07-31 11:39:26 +0200

    Early stopping added to NN training
    Introduced early stopping in the NN training procedure to prevent overfitting.
    - The training stops if the validation loss does not improve for a number of consecutive epochs equal to 20% of the total.
    - The model automatically restores the weights corresponding to the epoch with the lowest validation loss.
    - Predictions are now based on the best validation model instead of the final epoch.

--- NN_training.py
+++ NN_training.py
@@ -5,6 +5,9 @@ from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 import pandas as pd
 
+epochs = 200
+patience = int(0.2 * epochs)
+
 # Load preprocessed data
 X = np.load("X_features.npy")
 Y_labels = np.load("Y_labels.npy")
@@ -17,10 +20,9 @@ scaler_X = MinMaxScaler()
 X_train_scaled = scaler_X.fit_transform(X_train)
 X_test_scaled = scaler_X.transform(X_test)
 
-# Scale outputs
+# Scale outputs (trainin only)
 scaler_Y = MinMaxScaler()
 Y_train_scaled = scaler_Y.fit_transform(Y_train)
-Y_test_scaled = scaler_Y.transform(Y_test)
 
 # Define network
 net = tf.keras.models.Sequential([
@@ -29,3 +31,20 @@ net = tf.keras.models.Sequential([
     tf.keras.layers.Dense(32, activation='relu'),
     tf.keras.layers.Dense(Y_labels.shape[1], activation='linear'),
 ])
+
+# Compile
+net.compile(optimizer='adam', loss='mse', metrics=['mse'])
+
+# Early stopping callback
+early_stop = tf.keras.callbacks.EarlyStopping(
+    monitor = 'val_loss',           # monitor validation loss
+    patience = patience,            # stop if no improvement after 20% of total epochs
+    restore_best_weights = True     # restore weights from best epoch
+)
+
+# Train
+history = net.fit(X_train_scaled, Y_train_scaled, validation_split = 0.2, epochs=epochs, batch_size=32, verbose=1, callbacks=[early_stop])
+
+# Predict (rescale predictions)
+Y_pred_scaled = net.predict(X_test_scaled)
+Y_pred = scaler_Y.inverse_transform(Y_pred_scaled)
